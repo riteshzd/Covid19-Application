a49aab2ff02303506ecbaf222561d72b59c528b4
Ritesh <riteshdash305@gmail.com>
2020-04-24 20:52:35 +0530

    Updates

---
+++ precache-manifest.e6e47cf32a58dbecf83e94018f834fb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e14fcf8b48d8b10587616a78660ed250",
+    "url": "/Covid19-Application/index.html"
+  },
+  {
+    "revision": "e9a230dfb3da570b68e8",
+    "url": "/Covid19-Application/static/css/main.358db9b8.chunk.css"
+  },
+  {
+    "revision": "130f7c24971898e1c608",
+    "url": "/Covid19-Application/static/js/2.1beb2be5.chunk.js"
+  },
+  {
+    "revision": "e9a230dfb3da570b68e8",
+    "url": "/Covid19-Application/static/js/main.60b122f2.chunk.js"
+  },
+  {
+    "revision": "5c9be5c5ce69f8ec822d",
+    "url": "/Covid19-Application/static/js/runtime~main.c9a81e4d.js"
+  }
+]);
